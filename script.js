f775b4e3abb5d1dfd062f528e51d40dfe185de15
Grzegorz Gebala <grzegorz.gebala@yahoo.com>
2018-10-14 22:51:13 +0200

    small repairs

--- script.js
+++ script.js
@@ -22,21 +22,39 @@ var movies = [
 
 var Movie = React.createClass({
   propTypes: {
-    movies: React.PropTypes.object.isRequired,
+    movie: React.PropTypes.object.isRequired,
   },
 
   render: function() {
     return (
-      React.createElement('li', {key: movies.id},
-        React.createElement('h2', {}, movies.title),
-        React.createElement('p', {}, movies.desc),
-        React.createElement('img', {src: movies.poster} )
+      React.createElement('li', {}, 
+        React.createElement('h2', {}, this.props.movie.title),
+        React.createElement('p', {}, this.props.movie.desc),
+        React.createElement('img', {src: this.props.movie.poster} )
       )
     )
   }
 });
 
-var element = React.createElement(Movie, {key: movies.id, title: movies.title, desc: movies.desc, poster: movies.poster},);
+var MoviesList = React.createClass({
+  propTypes: {
+    data: React.PropTypes.array.isRequired,
+  },
+
+  render: function () {
+    var moviesList = this.props.data.map(function(item){
+      return (
+        React.createElement(Movie, {key: item.id, movie: item})
+      );
+    });
+
+    return (
+      React.createElement('ul', {}, moviesList)
+    );
+  }
+});
+
+var element = React.createElement(MoviesList, {data: movies});
 ReactDOM.render(element, document.getElementById('app'));
 
 /*
